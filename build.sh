5d4f66579b17c4bf3c933a17fa27402ff5b35c06
aelxna <aelxna@proton.me>
2025-01-12 13:30:41 -0600

    additional build step

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #! /usr/bin/env bash
 
-docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app ghcr.io/getzola/zola:v0.19.1 build
+docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app ghcr.io/getzola/zola:v0.19.2 build \
+&& \
+cp -r public /var/www/aelena
